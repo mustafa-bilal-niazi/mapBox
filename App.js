19c06d254b6fac0810cf8bbd52f2b6fa773f7979
Muhammad Mustafa Bilal Niazi <93573285+mustafa-bilal-niazi@users.noreply.github.com>
2023-04-19 16:50:27 +0500

    +,- and world, reset view added

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,TextInput , TouchableOpacity,value,KeyboardAvoidingView} from 'react-native';
+import { StyleSheet, Text,Image, View,TextInput , TouchableOpacity,value,KeyboardAvoidingView} from 'react-native';
 import  Mapbox from '@rnmapbox/maps';
 import * as Location from 'expo-location';
 
@@ -9,13 +9,22 @@ Mapbox.setAccessToken('sk.eyJ1IjoibXVzdGFmYTA0IiwiYSI6ImNsZ2twNXppYjFpMHYzaHQxan
 export default function App() {
 
   const [getLocation, setLocation] = useState([]); 
-  const [latitude,setLatitude]=useState(73)
-  const [longitude,setLongitude]=useState(33)
-  const [centerLat,setCenterLat]=useState(70)
-  const [centerLong,setCenterLong]=useState(33)
+  const [latitude,setLatitude]=useState(33)
+  const [longitude,setLongitude]=useState(73)
+  const [centerLat,setCenterLat]=useState(30)
+  const [centerLong,setCenterLong]=useState(73)
+  const [zoomLevel,setzoomLevel]=useState(16)
+
+  const zoomIn = () => {
+    setzoomLevel(zoomLevel+1)
+  }
+  const zoomOut = () => {
+    setzoomLevel(zoomLevel-1)
+  }
 
   const updateLatLong = () => {
     if(latitude!=null && longitude!=null){
+      setzoomLevel(16)
       if((longitude<= 180) && (longitude>= -180) && (latitude<= 90) && (latitude>= -90)) {
         console.log('updating center lat '+ latitude)
         console.log('updating center long '+ longitude)
@@ -78,8 +87,7 @@ export default function App() {
             <View style={{borderRadius: 12, backgroundColor: 'white', borderBottomColor: 'red', width: '25%'}}>
               <TextInput
                 style={{paddingLeft: 10,height: 50, color: 'black'}}
-                placeholder='latitude' placeholderTextColor='green
-                '
+                placeholder='latitude' placeholderTextColor='gray'
                 onChangeText={text => {
                   const value = parseFloat(text);
                   if (!isNaN(value)) {
@@ -141,6 +149,52 @@ export default function App() {
           <View style={{flexDirection: 'row'}}>
             <Text style={{color: 'white'}}> latitude</Text>
             <Text style={{color: 'white', marginLeft: 40}}>  longitude</Text>
+            <TouchableOpacity 
+              style={{
+                backgroundColor: '#1b1c1e',
+                borderRadius: 10,
+                marginLeft: 35,
+                padding: 8,
+                alignItems: 'center',
+                borderColor: 'green',
+                borderWidth: 4 }}
+              onPress={zoomIn}
+            >
+              <Text style={{color: 'white',fontSize: 20}}>
+                +
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+              style={{
+                backgroundColor: '#1b1c1e',
+                borderRadius: 10,
+                marginLeft: 10,
+                padding: 8,
+                alignItems: 'center',
+                borderColor: 'green',
+                borderWidth: 4 }}
+              onPress={zoomOut}
+            >
+              <Text style={{color: 'white',fontSize: 20}}>
+                -
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+              style={{
+                backgroundColor: '#1b1c1e',
+                borderRadius: 10,
+                marginLeft: 10,
+                padding: 8,
+                alignItems: 'center',
+                borderColor: 'green',
+                borderWidth: 4 }}
+              onPress={()=>setzoomLevel(0)}
+            >
+              <Text style={{color: 'white',fontSize: 15}}>
+                world
+              </Text>
+            </TouchableOpacity>
+
           </View>
       </View>
 
@@ -149,8 +203,10 @@ export default function App() {
           style={styles.map}
           styleURL={'mapbox://styles/mustafa04/clgkgr94r008901qtcri1e6m3'}>
           <Mapbox.Camera 
-            zoomLevel={17}
+            zoomLevel={zoomLevel}
             centerCoordinate={[centerLong,centerLat]}  />
+          <Mapbox.PointAnnotation coordinate={[centerLong,centerLat]} />
+
         </Mapbox.MapView>
       </View>
     </KeyboardAvoidingView>
